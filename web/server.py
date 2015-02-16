bdda4356d80340da6bb744b18d07a553d81af0dc
Robert Tingirica <tingirica.robert@gmail.com>
2015-02-16 13:32:28 +0200

    Added method for adding url to DB
    This adds a method in the Adgular frontend to add a url in the db
    and a route in the Flask backend.

--- web/server.py
+++ web/server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from pymongo import MongoClient
 
 import json
+import urllib2
 
 
 MONGODB_HOST = 'localhost'
@@ -22,11 +23,35 @@ def index():
     existing_urls = []
 
     for url in urls:
-        usr = {'name': str(url['name']),
-               'url': str(url['url'])}
+        usr = {'url': str(url['url'])}
         existing_urls.append(usr)
 
     return render_template("index.html", existing=json.dumps(existing_urls))
+
+@app.route('/add_url', methods=['GET', 'POST'])
+def add_url():
+    req_data = json.loads(request.data)
+    db = connection['mainAPP']
+    collection = db.urls
+    DBurl = collection.find_one({'url': req_data['url']})
+    url = req_data['url']
+    # check  what offer the user clicked and if he used it before
+
+    if request.method == 'POST':
+        try:
+            response = urllib2.urlopen(url)
+
+            if (DBurl and DBurl["url"]==response.url):
+                app.logger.debug(DBurl['offer'])
+                return "invalidUrl"
+            else:
+                req_data['url'] = response.url
+                collection.insert(req_data)
+                return "inserted"
+
+        except Exception as e:
+            app.logger.debug(e)
+            return "invalidUrl"
 
 if __name__ == '__main__':
     #remove debug=True for production!
--- web/static/dboxApp.js
+++ web/static/dboxApp.js
@@ -12,21 +12,42 @@ app.controller('firstController', ['$scope', '$http', function ($scope, $http) {
     $scope.button = 'free';
     $scope.showWarning = false;
 
-    $scope.getLocation = function() {
-        $scope.showWarning = false;
-        if (!$scope.newUrl) {
-            $scope.showWarning = true;
-            return;
-        }
+    $scope.getUrlElement = function(url) {
         var l = document.createElement("a");
-
-        l.href = $scope.newUrl;
-        // return l;
+        l.href = url;
+        return l;
     };
 
     $scope.isActive = function(route) {
         return route === $location.path();
     }
+
+    $scope.getLocation = function(){
+        $scope.showWarning = false;
+        $scope.showBadUrlWarning = false;
+
+        if (!$scope.newUrl) {
+            $scope.showWarning = true;
+            return;
+        }
+        
+        var obj = JSON.parse('{"url": "' + $scope.newUrl +'", "offer": "' + $scope.button + '"}');
+        console.log(obj);
+        console.log(obj);
+
+        $http({
+            method: "post",
+            url: "/add_url",
+            headers: {'Content-Type': "application/json"},
+            data: obj
+        }).success(function (response) {
+            if (response=="invalidUrl"){
+                $scope.showBadUrlWarning = true;
+                return;
+            }
+        });
+
+    };
 
 }]);
 
